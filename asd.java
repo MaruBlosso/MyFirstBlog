fb63a40c669b8b637be0a1e68c2988dbab8eec51
MaruBlosso <accccv01@gmail.com>
2024-08-10 20:31:22 +0900

    깃헙다시적응하기

---
+++ asd.java
@@ -0,0 +1,12 @@
+/**
+ * asd
+ */
+public class asd {
+
+    public static void main(String[] args) {
+        System.out.println("\\    /\\\n" + //
+                        " )  ( ')\n" + //
+                        "(  /  )\n" + //
+                        " \\(__)|");
+    }
+}
